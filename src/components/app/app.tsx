30e23588d852e7e1191d3a19a5072a6033a15b79
Natalia <tali154@yandex.ru>
2025-10-28 00:29:29 +0700

    added modal for ingredient

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,8 +12,13 @@ import {
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader, ProtectedRoute } from '@components';
-import { Routes, Route } from 'react-router-dom';
+import {
+  AppHeader,
+  IngredientDetails,
+  Modal,
+  ProtectedRoute
+} from '@components';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from '../../services/store';
 import { getAllIngredients, getIngredients } from '@slices';
@@ -22,73 +27,94 @@ import { Preloader } from '@ui';
 const App: FC = () => {
   const { isIngredientsLoading } = useSelector(getIngredients);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     dispatch(getAllIngredients());
   }, [dispatch]);
 
+  const closeModal = () => {
+    navigate('/');
+  };
+
+  const backgroundLocation = location.state?.background;
   return (
     <div className={styles.app}>
       <AppHeader />
       {isIngredientsLoading ? (
         <Preloader />
       ) : (
-        <Routes>
-          <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed' element={<Feed />} />
-          <Route
-            path='/login'
-            element={
-              <ProtectedRoute>
-                <Login />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/register'
-            element={
-              <ProtectedRoute>
-                <Register />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/forgot-password'
-            element={
-              <ProtectedRoute>
-                <ForgotPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/reset-password'
-            element={
-              <ProtectedRoute>
-                <ResetPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route path='/profile'>
+        <>
+          <Routes location={backgroundLocation || location}>
+            <Route path='/' element={<ConstructorPage />} />
+            <Route path='/ingredients/:id' element={<IngredientDetails />} />
+            <Route path='/feed' element={<Feed />} />
             <Route
-              index
+              path='/login'
               element={
                 <ProtectedRoute>
-                  <Profile />
+                  <Login />
                 </ProtectedRoute>
               }
             />
             <Route
-              path='orders'
+              path='/register'
               element={
                 <ProtectedRoute>
-                  <ProfileOrders />
+                  <Register />
                 </ProtectedRoute>
               }
             />
-          </Route>
-          <Route path='*' element={<NotFound404 />} />
-          {/* also modals here */}
-        </Routes>
+            <Route
+              path='/forgot-password'
+              element={
+                <ProtectedRoute>
+                  <ForgotPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/reset-password'
+              element={
+                <ProtectedRoute>
+                  <ResetPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route path='/profile'>
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='orders'
+                element={
+                  <ProtectedRoute>
+                    <ProfileOrders />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+            <Route path='*' element={<NotFound404 />} />
+          </Routes>
+          {backgroundLocation && (
+            <Routes>
+              <Route
+                path='/ingredients/:id'
+                element={
+                  <Modal title='Детали ингредиента' onClose={closeModal}>
+                    <IngredientDetails />
+                  </Modal>
+                }
+              />
+            </Routes>
+          )}
+        </>
       )}
     </div>
   );
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector } from '../../services/store';
+import { getIngredients } from '@slices';
+import { Params, useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams<Params>();
+  const { ingredients } = useSelector(getIngredients);
+
+  const ingredientData = ingredients.find((i) => i._id == id);
 
   if (!ingredientData) {
     return <Preloader />;
